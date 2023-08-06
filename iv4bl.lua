1d515733223fdcf6a31908a9700f86adf53be943
Bamskz <141552812+Bamskz@users.noreply.github.com>
2023-08-06 17:47:29 +0800

    Add files via upload

---
+++ iv4bl.lua
@@ -0,0 +1,7 @@
+return {
+"76.181.194.49", -- ValentineIn_December
+"49.145.200.92", -- SainenSkrrt
+"136.169.224.161", -- xuy_bati208
+"109.252.25.150", -- Katechka1976
+"23.120.206.92" -- jahniking2
+}
