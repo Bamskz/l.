0f34d715b96aa8a1f94fddc43eb1ce0be189707c
Bamskz <141552812+Bamskz@users.noreply.github.com>
2023-08-06 17:46:04 +0800

    Add files via upload

---
+++ hwidbl.lua
@@ -0,0 +1,6 @@
+return {
+"782023ED-11F1-4D44-8DD8-7FB0478D966D", -- ValentineIn_December
+"5087687A-B2FE-4683-B54E-7085902BB201", -- SainenSkrrt
+"ffdc5b85-6881-4ece-bb16-956000cd81ed", -- Katechka1976
+"39099fb3-7a8b-41e2-b915-ae3e01eb9881" -- jahniking2
+}
